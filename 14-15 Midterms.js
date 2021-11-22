b73edcd823b3bf9fc4d6a847837b0c05bc5d2c8a
Source Academy User <No public email provided>
2021-11-22 11:50:46 +0800

    Changes made from Source Academy

--- 14-15 Midterms.js
+++ 14-15 Midterms.js
@@ -1,40 +1,40 @@
-function A() {
-    return cond_a
-        ? do_y()
-        : do_x();
-}
+// function A() {
+//     return cond_a
+//         ? do_y()
+//         : do_x();
+// }
 
-function B() {
-    return cond_a
-        ? do_x()
-        : cond_b
-        ? do_x()
-        : do_y();
-}
+// function B() {
+//     return cond_a
+//         ? do_x()
+//         : cond_b
+//         ? do_x()
+//         : do_y();
+// }
 
-function C() {
-    return cond_a 
-        ? cond_b
-            ? do_x()
-            : do_y()
-        : do_y();
-}
+// function C() {
+//     return cond_a 
+//         ? cond_b
+//             ? do_x()
+//             : do_y()
+//         : do_y();
+// }
 
 function myfunc(a) {
-    return b => c => {
-        return a * b + c;
-    };
+    return b => c => a * b + c;
 }
 
-myfunc(3)(2)(1);
+// myfunc(3)(2)(1);
 
 function BST_min(bst) {
-    return is_null(tail(bst))
+    return is_null(bst)
+        ? Infinity
+        : is_null(head(tail(bst)))
         ? head(bst)
-        : BST_min(tail(bst));
+        : BST_min(head(tail(bst)));
 }
 
-function BST_find(x. bst) {
+function BST_find(x, bst) {
     return is_null(bst)
         ? false
         : x === head(bst)
@@ -48,11 +48,11 @@ function BST_to_list(bst) {
     if (is_null(bst)) {
         return null;
     }
-    let left = BST_to_list(tail(bst));
+    let left = BST_to_list(head(tail(bst)));
     let mid = head(bst);
-    let right = BST_to_list(tail(tail(bst)));
+    let right = BST_to_list(head(tail(tail(bst))));
     
-    return append(left, append(mid, right));
+    return append(left, pair(mid, right));
 }
 
 function find_ranks(lst) {
@@ -71,9 +71,48 @@ function rank_sort(lst) {
     return map(x => get_num(lst, ranks)(x), index);
 }
 
+// Q6
+
+function singles_to_english(d) {
+    return list_ref(list("", "one", "two", "three",
+
+"four", "five", "six",
+"seven", "eight", "nine"), d);}
+
+function tens_to_english(t) {
+return list_ref(list("", "ten", "twenty", "thirty",
+
+"forty", "fifty", "sixty",
+"seventy", "eighty", "ninety"), t);}
+
+function ten_to_nineteen_to_english(s) {
+return list_ref(list("ten", "eleven", "twelve", "thirteen",
+                         "fourteen", "fifteen", "sixteen",
+                         "seventeen", "eighteen",
+                         "nineteen"), s);
+}
+
+function power_of_thousand(n) {
+return list_ref(list("", "thousand", "million", "billion",
+
+"trillion", "quadrillion",
+"quintillion"), n);}
+
+// for each triplet of hundred/ten/single 
+function triplet_to_english(h, t, s) {
+const he = h > 0 ? singles_to_english(h) + " hundred" : ""; const te = t > 0 ? tens_to_english(t) : "";
+const se = s > 0 ? singles_to_english(s) : "";
+const tese = te === "" ? se
+                 : t === 1 ? ten_to_nineteen_to_english(s)
+                   : se === "" ? te : te + " " + se;
+return he === "" ? tese : tese === "" ? he
+             : he + " " + tese;
+}
+
+
 function number_to_digits(n) {
     return n < 10
-        ? list(n);
+        ? list(n)
         : pair(n % 10, number_to_digits(math_floor(n/10)));
 }
 
@@ -82,16 +121,15 @@ function triplets(digits) {
     if (length(digits) === 0) {
         return null;
     } else if (length(digits) === 1) {
-        return list( list(list_ref(digits, 0), 0, 0 );
+        return list( list(list_ref(digits, 0), 0, 0) );
     } else if (length(digits) === 2) {
         return list( list(list_ref(digits, 0), list_ref(digits, 1), 0) );
     } else {
-        
         pair(
             list( list_ref(digits, 0), 
                   list_ref(digits, 1), 
-                  list_ref(digits, 2) ) 
-            , triplets(tail(tail(tail(digits)))));   
+                  list_ref(digits, 2) ), 
+            triplets(tail(tail(tail(digits)))));   
     }
 
 }
@@ -108,7 +146,7 @@ function triplets_to_english(triplets) {
         let ct = head(xs);
         let ce = triplet_to_english(head(ct), head(tail(ct)), head(tail(tail(ct))));
         let thousands = power_of_thousand(n);
-        return ce + " " thousands + " " + helper(tail(xs), n - 1);
+        return ce + " " + thousands + " " + helper(tail(xs), n - 1);
     }
     
     return helper(ordered, len - 1);
@@ -119,7 +157,7 @@ function number_to_english(n) {
     return triplets_to_english(triplets(number_to_digits(n)));
 }
 
-
+triplets_to_english(null);
 
 
 
